88a120322cc13f7b5f12523225bae78c6f586730
Antonio <amilbar951@g.educaand.es>
2022-09-26 10:00:40 +0200

    versión 2

--- sumaNumeros.php
+++ sumaNumeros.php
@@ -1,12 +1,15 @@
 <?php
     //Sumar dos números escritos en el url
+    //Versión 2: Ahora está en negrita
+
     $a = $_GET['a'];
     $b = $_GET['b'];
     $c = $a + $b;
 
     if(isset($a) && isset($b))
     {
-        echo "$a + $b = $c";
+        echo "<h1>Calculadora para sumar dos números</h1>";
+        echo "$a + $b = <strong>$c</strong>";
     }
     else
     {
